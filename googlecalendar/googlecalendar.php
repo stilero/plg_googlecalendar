b1225bb74c5647843ca1ac411e522b9236420671
Daniel Eliasson <daniel@stilero.com>
2012-12-17 14:02:27 +0100

    Fixed date formatting

--- googlecalendar/googlecalendar.php
+++ googlecalendar/googlecalendar.php
@@ -28,10 +28,10 @@ class Googlecalendar{
     private $imageSrc;
     
     public function __construct($title, $startDate, $endDate, $location='', $description='', $websiteName='', $websiteUrl='') {
-        $this->imageSrc = JPATH_ROOT.self::$imageSrcPath;
+        $this->imageSrc = JURI::base().self::$imageSrcPath;
         $this->title = $title;
-        $this->startDate = $startDate;
-        $this->endDate = $endDate;
+        $this->startDate = $this->formatDate($startDate);
+        $this->endDate = $this->formatDate($endDate);
         $this->location = $location;
         $this->description = $description;
         $this->websiteName = $websiteName;
@@ -41,17 +41,26 @@ class Googlecalendar{
     public function eventLink(){
         $html = '<a title="Add to Google Calendar" class="addgooglecal" target="_blank" href="'.
                 self::$url.'action=TEMPLATE'.
-                '&text='.$this->title.
+                '&text='.urlencode($this->title).
                 '&dates='.$this->startDate.'/'.$this->endDate.
-                '&location='.$this->location.
-                '&details='.$this->description.
+                '&location='.urlencode($this->location).
+                '&details='.urlencode(strip_tags($this->description)).
                 '&trp=true'.
-                '&sprop=website:'.$this->websiteUrl.
-                '&sprop=name:'.$this->websiteName.
+                '&sprop=website:'.urlencode($this->websiteUrl).
+                '&sprop=name:'.urlencode($this->websiteName).
                 '">'.
                 '<img src="'.$this->imageSrc.'">'.
                 '</a>';
         return $html;
-                
+    }
+    
+    private function formatDate($date){
+        $date =& JFactory::getDate($date);
+        $formattedDate = $date->toISO8601();
+        $dateWithoutSlashes = str_replace('-', '', $formattedDate);
+        $dateWithoutSemicolon = str_replace(':', '', $dateWithoutSlashes);
+        $dateWithoutTimeAdd = str_replace('+0000', '', $dateWithoutSemicolon);
+        return $dateWithoutTimeAdd.'';
+        
     }
 }
